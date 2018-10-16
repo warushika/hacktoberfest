44a318bc3601d2a6a4574ca2f18a6917083375e2
warushika <37923569+warushika@users.noreply.github.com>
2018-10-16 23:50:28 +0530

    add whales index file

---
+++ contribution/warushika/index.php
@@ -0,0 +1,292 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="css/fa/css/font-awesome.min.css">
+  <link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Bree+Serif" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+  <script src="js/jquery-3.2.1.min.js"></script>
+  <script src="js/propper.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/interactive.js"></script>
+
+</head>
+
+<body>
+ 
+<div class="container-fluid">
+ 
+<!-- navigation bar -->
+  
+
+  <nav class="navbar navbar-expand-lg navbar-dark  fixed-top" style="position:fixed; z-index:1">
+      
+        <a class="navbar-brand" href="#">MAKEYOURRIDE</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item active">
+              <a class="nav-link f2 " href="#">HOME
+                <span class="sr-only ">(current)</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link " href="#">ABOUT US</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">SERVICES</a>
+            </li>
+           
+            <li class="nav-item">
+              <a class="nav-link" href="#">SIGN IN</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">RESERVE</a>
+            </li>
+          </ul>
+        </div>
+     
+    </nav>
+
+   
+  
+      
+
+      <div class="cover">
+        <div class="row">
+        
+         </div>
+      </div>
+
+       <div class="cover_background">
+        <div class="row">
+        
+         </div>
+      </div>
+
+       <div class="cover_content">
+         <header class="masthead text-center text-white d-flex">
+      <div class="container my-auto">
+        <div class="row">
+          <div class="col-lg-10 mx-auto">
+            <h1 class="text-uppercase ">
+              <strong>RESERVE YOUR WHALE WATCHING TOURS </strong>
+            </h1>
+            <hr>
+          </div>
+          <div class="col-lg-8 mx-auto">
+            <p class="text-faded mb-5">Start Bootstrap can help you build better websites using the Bootstrap CSS framework! Just download your template and start going, no strings attached!</p>
+            <a class="btn btn-outline-primary btn-xl js-scroll-trigger" href="#about">GET STARTED</a>
+          </div>
+        </div>
+      </div>
+    </header>
+         </div>
+      
+
+<div class="about">
+   <h2 class="text-uppercase text-center">
+              <strong>we are royal tours mirissa</strong>
+            </h2>
+            <br>
+            <h5 class="text-center">We offer you the best whale and dolphin watching boat (comfortable/biggest and speed) with all the facilities from royal tours mirissa . From the beginning to the end of the journey.</h5>
+            <center>  <a class="btn btn-outline-warning btn-xl js-scroll-trigger" href="#about">Find Out More</a></center>
+
+</div>
+ <br> <br>
+<div class="container">
+  <div class="row">
+
+    <div class="col-md-6 whale_watching">
+      
+        <img src="images/ffs.jpg" alt="" class=" dolphin_watching_img">
+          <h2 class="text-uppercase text-center headwhale ">
+              <strong>Arange your whale watching tours</strong>
+            </h2>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+      consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+      cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+      proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      <br> <br>
+      <a class="btn btn-outline-warning btn-sm js-scroll-trigger" href="#about"><b>REGISTER</b></a>
+      <a class="btn btn-outline-warning btn-sm js-scroll-trigger" href="#about"><b>REGISTER</b></a>
+    
+    
+    </div>
+    <div class="col-md-6 dolphin_watching">
+        <img src="images/lk.jpg" alt="" class="dolphin_watching_img">
+          <h2 class="text-uppercase text-center headwhale ">
+              <strong>Arange your dolphin watching tours</strong>
+            </h2>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+      consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+      cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+      proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      <br> <br>
+       <a class="btn btn-outline-warning btn-sm js-scroll-trigger" href="#about"><b>REGISTER</b></a>
+      <a class="btn btn-outline-warning btn-sm js-scroll-trigger" href="#about"><b>REGISTER</b></a>
+    </div>
+  </div>
+</div>
+ <br> <br> 
+ <!-- slider -->
+ <div class="container">
+ <div  class="row">
+
+        <div id="carouselExampleIndicators" class="carousel slide carousel-fade col-md-7" data-ride="carousel" data-interval="4000" >
+          <ol class="carousel-indicators">
+            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+          </ol>
+          <div class="carousel-inner " role="listbox">
+            <div class="carousel-item active customc ">
+              <img class="d-block img-fluid customslide imgc" src="images/bb.jpeg" alt="First slide">
+            
+              
+            </div>
+            <div class="carousel-item customc ">
+              <img class="d-block img-fluid customslide imgc" src="images/ww.jpeg" alt="Second slide">
+            
+               
+            </div>
+            <div class="carousel-item customc ">
+              <img class="d-block img-fluid customslide imgc" src="images/sss.jpg" alt="Third slide">
+             
+            </div>
+          </div>
+        
+</div>
+   
+    
+      <!-- slider -->
+      <div id="carouselExampleIndicators" class="carousel slide carousel-fade col-md-5" data-ride="carousel" data-interval="4000" >
+       <ol class="carousel-indicators">
+            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+        </ol>
+          <div class="carousel-inner " role="listbox">
+              <div class="carousel-item active customc  customcd">
+                  <center><h1> JOIN WITH US</h1>
+                   Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et 
+                    <br>
+
+                 <a class="btn btn-outline-warning btn-sm js-scroll-trigger" href="#about"><b>REGISTER</b></a>
+                 <a class="btn btn-outline-warning btn-sm js-scroll-trigger" href="#about">LOGIN</a>
+
+                 </center>
+              </div>
+            <div class="carousel-item customc  customcd">
+                <center><h1> JOIN WITH US</h1>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et 
+                    <br>
+
+                     <a class="btn btn-outline-warning btn-sm js-scroll-trigger" href="#about"><b>REGISTER</b></a>
+                      <a class="btn btn-outline-warning btn-sm js-scroll-trigger" href="#about">LOGIN</a>
+
+                </center>
+             </div>
+            <div class="carousel-item customc  customcd">
+               <center><h1> JOIN WITH US</h1>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                   tempor incididunt ut labore et 
+                   <br>
+
+                 <a class="btn btn-outline-warning btn-sm js-scroll-trigger" href="#about">REGISTER</a>
+                 <a class="btn btn-outline-warning btn-sm js-scroll-trigger" href="#about">LOGIN</a>
+
+              </center>
+            </div>
+          </div>
+        </div>
+    </div>
+  </div>
+ 
+ 
+
+
+
+ <div class="services">
+    
+       <h2 class="text-uppercase text-center cwhite" >
+              <strong>SERVICES</strong>
+            </h2>
+            <br>
+            <h5 class="text-center cwhite">We offer you the best whale and dolphin watching boat (comfortable/biggest and speed) with all the facilities from royal tours mirissa . From the beginning to the end of the journey.</h5>
+            <br><br>
+          <div class="container">
+            <div class="row">
+              <div class="col-md-5  serviceblock">
+                <center><h2>Arange transport</h2></center>
+                <img src="images/dv.jpg" class="van"alt="">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                consequat. 
+                  <br> <br>
+                  <center>  <a class="btn btn-secondary btn-xl js-scroll-trigger" href="#about">Find Out More</a></center>
+
+    
+              </div>
+              <div class="col-md-2">
+                
+              </div>
+              <div class="col-md-5 serviceblock">
+                <center><h2>Book your Residance</h2></center>
+                 <img src="images/sv.png" class="van"alt="">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                consequat.
+                 <br> <br> 
+                  <center>  <a class="btn btn-secondary btn-xl js-scroll-trigger" href="#about">Find Out More</a></center>
+    
+              </div>
+            </div>
+          </div>
+  </div>
+
+ <div id="map">
+   
+ </div>
+    
+
+
+
+
+
+
+
+    <script>
+// Initialize and add the map
+function initMap() {
+  // The location of Uluru
+  var uluru = {lat: 5.947780, lng: 80.448143
+
+};
+  // The map, centered at Uluru
+  var map = new google.maps.Map(
+      document.getElementById('map'), {zoom: 15, center: uluru});
+  // The marker, positioned at Uluru
+  var marker = new google.maps.Marker({position: uluru, map: map});
+}
+    </script>
+    <script async defer
+  src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDIr_1mumk0EcpdtP_hT0UCMVTX4b7xy0s&callback=initMap">
+</script>
+
+</div>
+</body>
+</html>
